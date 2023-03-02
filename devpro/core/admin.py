446d8f0eec43a1b2ec6e1904a16891fdfe514264
Walison Filipe <walisonfilipe@hotmail.com>
2023-03-02 10:35:09 -0300

    Adiciona ação para exportar livros como csv

--- devpro/core/admin.py
+++ devpro/core/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
+from django.shortcuts import redirect, resolve_url
 
 from .models import Author, Book
 
 
-admin.site.register([Author, Book])
+admin.site.register([Author])
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'publication_year', 'get_authors')
+    search_fields = ('name', 'authors__name')
+    ordering = ('publication_year', 'name')
+    list_filter = ('publication_year',)
+    actions = ['export_csv']
+
+    @admin.display(description='Authors')
+    def get_authors(self, obj):
+        return ', '.join(author.name for author in obj.authors.all())
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related('authors')
+
+    @admin.action(description='Exportar como csv')
+    def export_csv(self, request, queryset):
+        ids = queryset.values_list('id', flat=True)
+        return redirect(
+            resolve_url('core:export'), ids=','.join(str(i) for i in ids)
+        )
--- devpro/core/urls.py
+++ devpro/core/urls.py
@@ -7,5 +7,6 @@ app_name = 'core'
 urlpatterns = [
     path('authors/', views.authors, name='list-authors'),
     path('books/', views.book_list_create, name='create-book'),
-    path('books/<int:pk>/', views.book_read_update_delete, name='read-update-delete-book')
+    path('books/<int:pk>/', views.book_read_update_delete, name='read-update-delete-book'),
+    path('books/export/', views.export_csv, name='export')
 ]
